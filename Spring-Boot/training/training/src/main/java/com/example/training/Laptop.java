b331977a5a0fcbe88cb0d6602802ef1b57648063
adityakannan19 <62856386+adityakannan19@users.noreply.github.com>
2024-02-01 15:53:29 +0530

    Add files via upload

---
+++ Spring-Boot/training/training/src/main/java/com/example/training/Laptop.java
@@ -0,0 +1,36 @@
+package com.example.training;
+
+import org.springframework.stereotype.Component;
+
+@Component("lap1")
+public class Laptop {
+
+	int lid;
+	String brand;
+
+	public int getLid() {
+		return lid;
+	}
+
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	@Override
+	public String toString() {
+		return "Laptop [lid=" + lid + ", brand=" + brand + "]";
+	}
+	
+	public void compile() {
+		System.out.println("Compiling...");
+	}
+
+}
