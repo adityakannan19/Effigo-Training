673f19834e44211802412f908afcadf94ffacbd0
adityakannan19 <62856386+adityakannan19@users.noreply.github.com>
2024-03-01 20:28:58 +0530

    Update CourseService.java

--- Spring-Boot/LearningPortal/src/main/java/com/example/learningportal/service/CourseService.java
+++ Spring-Boot/LearningPortal/src/main/java/com/example/learningportal/service/CourseService.java
@@ -4,8 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.example.learningportal.entity.Course;
